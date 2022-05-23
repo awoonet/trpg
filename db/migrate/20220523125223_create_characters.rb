8b81bc8707b95112d04b4673ea3c3b2169eae6b9
dankild <dan.kild@speedandfunction.com>
2022-05-23 16:20:37 +0300

    make fields for migrations

--- db/migrate/20220523125223_create_characters.rb
+++ db/migrate/20220523125223_create_characters.rb
@@ -1,6 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[7.0]
   def change
     create_table :characters do |t|
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20220523125246_create_games.rb
+++ db/migrate/20220523125246_create_games.rb
@@ -1,6 +1,7 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
+      t.string :name
 
       t.timestamps
     end
