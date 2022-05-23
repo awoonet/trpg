1b51b06e2bfb669b60f15d01e6724b9f4f509d50
dankild <dan.kild@speedandfunction.com>
2022-05-23 19:16:09 +0300

    set routes hierarhy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :locations
-  resources :games
-  resources :characters
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "games#index"
+
+  resources :games do
+    resources :locations do
+      resources :posts
+    end
+    resources :characters
+  end
+  resources :users
 end
