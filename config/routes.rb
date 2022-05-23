be859bbd65165fb1efb26c897b6cc3efe108529e
dankild <dan.kild@speedandfunction.com>
2022-05-23 19:13:01 +0300

    add all views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :locations
+  resources :games
+  resources :characters
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
