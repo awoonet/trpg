4e1620d4021fbcc6487e5123f3c5f43a265d1568
dankild <dan.kild@speedandfunction.com>
2022-05-29 21:23:35 +0300

    add helpers for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def nav_link path, name
+    tag.li class: "nav-item" do
+      link_to name, path, class: "nav-link"
+    end
+  end
+
+  def avatar_full record
+    image_tag(
+      record.avatar.file.file.gsub(/(.+)(public)/, ''), 
+      class: "img-fluid", alt: "#{record.name}'s avatar"
+    ) if record.avatar.file
+  end
+
+  def no_nsfw_without_login game
+    if current_user && game.nsfw
+      true
+    elsif !game.nsfw
+      true
+    else
+      false
+    end
+  end
 end
