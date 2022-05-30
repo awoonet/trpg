3a62eabf9662baabeabea6aae6934ea8ade355f2
dankild <dan.kild@speedandfunction.com>
2022-05-30 10:23:41 +0300

    new fields added to form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,27 +2,31 @@ class GamesController < ApplicationController
   before_action :set_vars
 
   private
-    # Only allow a list of trusted parameters through.
-    def model_params
-      params.require(:game).permit(:name, :description, :avatar)
-    end
 
-    def set_vars
-      @model = Game.signed_in?(current_user)
-      @index_path = games_path
-      @index_url = games_url
-      @new_path = new_game_path
-      @view = @model.name.pluralize.downcase
-      if params[:id]
-        @record = @model.find(params[:id])
-        @show_path = game_path(@record)
-        @edit_path = edit_game_path(@record)
-        @items = Item.for_game(params[:id])
-        @skills = Skill.for_game(params[:id])
-      end
+  # Only allow a list of trusted parameters through.
+  def model_params
+    params.require(:game).permit(
+      :name, :description, :avatar, 
+      :nsfw, :whitelisted, admins: [],
+      whitelist: [], blacklist: [])
+  end
+  
+  def set_vars
+    @model = Game.signed_in?(current_user)
+    @index_path = games_path
+    @index_url = games_url
+    @new_path = new_game_path
+    @view = @model.name.pluralize.downcase
+    if params[:id]
+      @record = @model.find(params[:id])
+      @show_path = game_path(@record)
+      @edit_path = edit_game_path(@record)
+      @items = Item.for_game(params[:id])
+      @skills = Skill.for_game(params[:id])
     end
+  end
 
-    def show_path
-      game_path(@record)
-    end
+  def show_path
+    game_path(@record)
+  end
 end
