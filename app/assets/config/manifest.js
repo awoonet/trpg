810785f517908c1de98ff5ec6f00b18320909db7
dankild <dan.kild@speedandfunction.com>
2022-05-30 20:47:42 +0300

    added different themes to assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,14 @@
 //= link_tree ../builds
 
 //= link bootstrap.bundle.min.js
-//= link bootstrap.min.css
+//= link bootstrap-darkly.min.css
+//= link bootstrap-default.min.css
+//= link bootstrap-flatly.min.css
+//= link bootstrap-morph.min.css
+//= link bootstrap-sandstone.min.css
+//= link bootstrap-sketchy.min.css
+//= link bootstrap-solar.min.css
+//= link bootstrap-vapor.min.css
 
 //= link slimselect.min.js
 //= link slimselect.min.css
