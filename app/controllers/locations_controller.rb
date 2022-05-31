5d6dc5d31c35f32335ad4d380bcbc1dc86c011c9
dankild <dan.kild@speedandfunction.com>
2022-05-31 22:51:45 +0300

    refactor locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,15 +1,8 @@
 class LocationsController < ApplicationController
   before_action :set_vars
 
-  # GET /model
-  def index
-    @records = @model.where(game_id: params[:game_id]).all
-    @breadcrumb = { "#{home_button}": 'active' }
-    render "models/#{@view}/index"
-  end
-
   # GET /model/1
-  def show
+  def show 
     @breadcrumb = { 
       "Games": games_path, 
       "#{@game.name}": game_path(@game),
@@ -18,25 +11,29 @@ class LocationsController < ApplicationController
   end
 
   private
-    # Only allow a list of trusted parameters through.
-    def model_params
-      params.require(:location).permit(:name, :game_id, :avatar)
-    end
 
-    def set_vars
-      @model = Location
-      @index_path = game_locations_path
-      @index_url = game_locations_url
-      @new_path = new_game_location_path
-      @view = @model.name.pluralize.downcase
-      if params[:id]
-        @record = @model.find(params[:id])
-        @show_path = game_location_path(@record)
-        @edit_path = edit_game_location_path(@record)
-      end
-    end
+  # Only allow a list of trusted parameters through.
+  def model_params
+    params.require(:location).permit(
+      :name, :avatar, :description, 
+      :game_id
+    )
+  end
 
-    def show_path
-      game_location_path(@record, game_id: params[:game_id])
+  def set_vars
+    @model = Location
+    @index_path = game_locations_path
+    @index_url = game_locations_url
+    @new_path = new_game_location_path
+    @view = @model.name.pluralize.downcase
+    if params[:id]
+      @record = @model.find(params[:id])
+      @show_path = game_location_path(@record)
+      @edit_path = edit_game_location_path(@record)
     end
+  end
+
+  def show_path
+    game_location_path(@record, game_id: params[:game_id])
+  end
 end
