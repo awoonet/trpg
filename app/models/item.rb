5444c14b5833866639dad2afc81a8a1daf049c3f
dankild <dan.kild@speedandfunction.com>
2022-05-31 22:54:00 +0300

    delete items and skills models

--- app/models/item.rb
+++
@@ -1,9 +0,0 @@
-class Item < ApplicationRecord
-  belongs_to :game
-
-  validates :name, presence: true
-
-  scope :for_game, ->(id) do 
-    where(game_id: id)
-  end
-end
--- app/models/skill.rb
+++
@@ -1,9 +0,0 @@
-class Skill < ApplicationRecord
-  belongs_to :game
-
-  validates :name, presence: true
-
-  scope :for_game, ->(id) do 
-    where(game_id: id)
-  end
-end
