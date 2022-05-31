8fe2c7c38ade04bdd579f33e70961611221be24e
dankild <dan.kild@speedandfunction.com>
2022-05-31 22:50:36 +0300

    add defaults to location relations

--- db/migrate/20220531151830_add_locations.rb
+++ db/migrate/20220531151830_add_locations.rb
@@ -4,8 +4,8 @@ class AddLocations < ActiveRecord::Migration[7.0]
       t.string :name, null: false
       t.text :description
       t.string :avatar
-      t.bigint :parent_location_id
-      t.bigint :child_locations_ids, array: true
+      t.bigint :parent_location_id, default: nil
+      t.bigint :child_locations_ids, array: true, default: []
 
       t.belongs_to :game
 
