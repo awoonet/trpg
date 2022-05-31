f631f5b84df7458f5ff09d9117749f678f0a1782
dankild <dan.kild@speedandfunction.com>
2022-05-31 22:54:21 +0300

    add scope and methods to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,20 @@ class Location < ApplicationRecord
   validates :name, presence: true
 
   mount_uploader :avatar, AvatarUploader
+
+  scope :for_game, -> (game_id) do
+    Location.where(game_id: game_id, parent_location_id: nil)
+  end
+
+  def parent_location
+    if parent_location_id
+      Location.find_by(id: parent_location_id)
+    end
+  end
+
+  def child_locations
+    if child_locations_ids
+      Locations.where(id: child_locations_ids)
+    end
+  end
 end
