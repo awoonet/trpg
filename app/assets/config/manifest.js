851704f623ea458aad1e4785fef3103b8f6d420b
dankild <dan.kild@speedandfunction.com>
2022-06-03 00:34:27 +0300

    added jquery

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,12 @@
 //= link_tree ../images
 //= link_tree ../builds
 
+
+//= link jquery.min.js
 //= link bootstrap.bundle.min.js
+//= link chosen.jquery.min.js
+
+
+//= link chosen.min.css
 //= link_tree ../stylesheets/bootstrap
 //= link_tree ../stylesheets/fontawesome
-
-//= link slimselect.min.js
-//= link slimselect.min.css
-
