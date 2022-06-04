ad9565bb4dc679cb6976b8e7a22dcec29751a1fb
dankild <dan.kild@speedandfunction.com>
2022-06-04 18:56:45 +0300

    deleted assets (used cdn)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,12 +1,2 @@
 //= link_tree ../images
 //= link_tree ../builds
-
-
-//= link jquery.min.js
-//= link bootstrap.bundle.min.js
-//= link chosen.jquery.min.js
-
-
-//= link chosen.min.css
-//= link_tree ../stylesheets/bootstrap
-//= link_tree ../stylesheets/fontawesome
