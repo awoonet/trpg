978c5c1a79aa39c58f435a243fd6e51856bce064
dankild <dan.kild@speedandfunction.com>
2022-06-04 19:01:30 +0300

    migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_152941) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_152948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,26 +21,27 @@ ActiveRecord::Schema.define(version: 2022_05_31_152941) do
     t.string "race"
     t.string "profession"
     t.integer "health"
-    t.integer "mana"
-    t.integer "strength"
-    t.integer "stamina"
-    t.integer "accuracy"
-    t.integer "agility"
-    t.integer "intelligence"
-    t.integer "wisdom"
-    t.integer "will"
-    t.integer "charm"
-    t.integer "money"
-    t.bigint "inventory", array: true
+    t.integer "max_health"
+    t.integer "energy"
+    t.integer "max_energy"
+    t.integer "strength", default: 0
+    t.integer "stamina", default: 0
+    t.integer "accuracy", default: 0
+    t.integer "agility", default: 0
+    t.integer "intelligence", default: 0
+    t.integer "wisdom", default: 0
+    t.integer "will", default: 0
+    t.integer "charm", default: 0
+    t.integer "money", default: 0
+    t.bigint "items", array: true
     t.bigint "skills", array: true
     t.text "description"
     t.text "biography"
     t.bigint "game_id"
     t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_characters_on_game_id"
-    t.index ["name"], name: "index_characters_on_name", unique: true
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
@@ -54,8 +54,11 @@ ActiveRecord::Schema.define(version: 2022_05_31_152941) do
     t.bigint "admins", array: true
     t.bigint "whitelist", array: true
     t.bigint "blacklist", array: true
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "max_health"
+    t.integer "max_energy"
+    t.integer "max_stat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_games_on_name", unique: true
   end
 
@@ -63,12 +66,10 @@ ActiveRecord::Schema.define(version: 2022_05_31_152941) do
     t.string "name", null: false
     t.string "kind"
     t.text "description"
-    t.string "type"
     t.bigint "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_items_on_game_id"
-    t.index ["name"], name: "index_items_on_name", unique: true
   end
 
   create_table "locations", force: :cascade do |t|
@@ -76,10 +77,10 @@ ActiveRecord::Schema.define(version: 2022_05_31_152941) do
     t.text "description"
     t.string "avatar"
     t.bigint "parent_location_id"
-    t.bigint "child_locations_ids", array: true
+    t.bigint "child_locations_ids", default: [], array: true
     t.bigint "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_locations_on_game_id"
   end
 
@@ -88,10 +89,20 @@ ActiveRecord::Schema.define(version: 2022_05_31_152941) do
     t.bigint "location_to"
     t.bigint "location_id"
     t.bigint "character_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["character_id"], name: "index_posts_on_character_id"
     t.index ["location_id"], name: "index_posts_on_location_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "kind"
+    t.text "description"
+    t.bigint "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_skills_on_game_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -102,8 +113,8 @@ ActiveRecord::Schema.define(version: 2022_05_31_152941) do
     t.boolean "is_admin", default: false
     t.string "crypted_password"
     t.string "salt"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
