566b30793e72820f1ab376cf3157a6ab32f48585
dankild <dan.kild@speedandfunction.com>
2022-06-04 18:55:04 +0300

    add 'faker' gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,39 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create(name: "Katsu", password: "q1w2e3r4", password_confirmation: "q1w2e3r4", email: "qqq@qqq.qqq", is_admin: true)
+require 'faker'
+
+user = User.create(name: Faker::Name.name, password: "q1w2e3r4", password_confirmation: "q1w2e3r4", email: "qqq@qqq.qqq", is_admin: true)
 
 6.times do |i|
-  game = Game.create(name: "Nyan #{rand}", admins: [user.id])
+  game = Game.create(
+    name: Faker::Game.title, 
+    admins: [user.id],
+    description: Faker::Books::Lovecraft.sentences.join(' '),
+    avatar: Faker::Placeholdit.image(size: '200x300', format: 'jpg'))
   game.update(whitelisted: true, whitelist: [user.id]) if i % 3 == 0
   game.update(nsfw: true) if i % 2 == 0
 
   5.times do |j|
-    item = Item.create(name: "Item #{rand}", kind: "artifact", description: "Powerful artifact", game_id: game.id)
-    skill = Skill.create(name: "Skill #{rand}", kind: "elemental", description: "Fire Magic", game_id: game.id)
+    item = Item.create(name: Faker::Games::DnD.melee_weapon, kind: "artifact", description: Faker::Books::Lovecraft.sentences.join('\n'), game_id: game.id)
+    skill = Skill.create(name: Faker::Games::Witcher.sign, kind: "elemental", description: Faker::Books::Lovecraft.sentences.join('\n'), game_id: game.id)
 
     char = Character.create(
       game_id: game.id, user_id: user.id,
-      name: "Feris #{rand}", age: 20, race: "Neko", profession: "Archer", money: 111,  
+      name: Faker::Games::Witcher.character, age: 20, race: Faker::Games::DnD.race, profession: Faker::Games::DnD.klass, money: 111,  
       health: 11, max_health: 15, strength: 2, stamina: 2, accuracy: 2, agility: 2, 
       energy: 11, max_energy: 15, intelligence: 2, wisdom: 2, will: 2, charm: 2,
-      description: "Brave Catgirl", biography: "Hard childhood", items: [item.id], skills: [skill.id]
+      description: Faker::Books::Lovecraft.sentences.join(' '), biography: Faker::Books::Lovecraft.sentences.join(' '), items: [item.id], skills: [skill.id],
+      avatar: Faker::Placeholdit.image(size: '200x300', format: 'jpg')
     )
     loc  = Location.create(
       game_id: game.id,
-      name: "Temple #{rand}", 
-      description: "Gorgeous temple"
+      name: Faker::Fantasy::Tolkien.location, 
+      description: Faker::Books::Lovecraft.sentences.join(' '),
+      avatar: Faker::Placeholdit.image(size: '200x300', format: 'jpg')
     )
     5.times do
-      Post.create(content: "#{rand}", character_id: char.id, location_id: loc.id)
+      Post.create(content: Faker::Books::Dune.quote, character_id: char.id, location_id: loc.id)
     end
   end
 end
