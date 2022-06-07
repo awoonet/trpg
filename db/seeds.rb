6c0b6067b3d3f6638566ec15142ec50bf19fcf0f
dankild <dan.kild@speedandfunction.com>
2022-06-07 13:39:50 +0300

    remade seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,19 +21,18 @@ user = User.create(name: Faker::Name.name, password: "q1w2e3r4", password_confir
   game.update(nsfw: true) if i % 2 == 0
 
   6.times do |j|
-    item = Item.create(name: Faker::Games::DnD.melee_weapon, kind: "artifact", description: Faker::Books::Lovecraft.sentences.join('\n'), game_id: game.id)
-    skill = Skill.create(name: Faker::Games::Witcher.sign, kind: "elemental", description: Faker::Books::Lovecraft.sentences.join('\n'), game_id: game.id)
+    item = Item.create(name: Faker::Games::DnD.melee_weapon, kind: "artifact", description: Faker::Books::Lovecraft.sentences.join(' '), game_id: game.id)
+    skill = Skill.create(name: Faker::Games::Witcher.sign, kind: "elemental", description: Faker::Books::Lovecraft.sentences.join(' '), game_id: game.id)
 
     max_health = rand(10..game.max_health)
     max_energy = rand(10..game.max_energy)
     char = Character.create(
       game_id: game.id, user_id: user.id,
       name: Faker::Games::Witcher.character, age: 20, race: Faker::Games::DnD.race, profession: Faker::Games::DnD.klass, money: 111,  
-      health: rand(1..max_health), max_health: max_health,  
-      energy: rand(1..max_energy), max_energy: max_energy, 
+      health: rand(1..max_health), max_health: max_health, energy: rand(1..max_energy), max_energy: max_energy,
       strength: rand(1..game.max_stat), stamina: rand(1..game.max_stat), accuracy: rand(1..game.max_stat), agility: rand(1..game.max_stat), 
-      intelligence: 2, wisdom: 2, will: 2, charm: 2, skill_ids: [skill.id],
-      item_ids: [item.id],
+      intelligence: rand(1..game.max_stat), wisdom: rand(1..game.max_stat), will: rand(1..game.max_stat), charm: rand(1..game.max_stat),
+      item_ids: [item.id], skill_ids: [skill.id],
       description: Faker::Books::Lovecraft.sentences.join(' '), biography: Faker::Books::Lovecraft.sentences.join(' '),
       avatar: Faker::Placeholdit.image(size: '200x300', format: 'jpg')
     )
